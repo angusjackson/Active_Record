8f45516179d0c2c723cd1aacff3a8455f60f40a1
doubletong <twotong@gmail.com>
2013-10-24 14:42:25 +0800

    2013-10-24

---
+++ WebUI/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //create your new user and set properties
+        Person myPerson = new Person();
+        myPerson.Username = "name";
+        myPerson.Password = "secret"; 
+        try
+        {
+            //try to save myPerson to database
+            myPerson.Create();
+        }
+        catch (Exception err) { string errReport = "Failed trying to save user"; errReport += err.Message; System.Diagnostics.Debug.Write(errReport); }
+    }
+}
